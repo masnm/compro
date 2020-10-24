05e0d5f711bcf8b728924c67880d5b506c6bda64
Ahmmed Sakib Noman <noman@DESKTOP-40A5I0B.localdomain>
2020-10-24 14:20:31 +0600

    update on core maybe on Makefiel also on Template

--- Makefile
+++ Makefile
@@ -5,17 +5,16 @@ reset:
 	touch in1 in2 in3
 	cp template main.cpp
 c:
-	g++ -std=c++17 -Wall main.cpp
+	g++ -g -std=c++17 -Wall main.cpp
 	rm a.out
-t1:
-	g++ -std=c++17 -Wall main.cpp
-	a.out < in1
+r:
+	g++ -g -std=c++17 -Wall main.cpp
+	./a.out
 	rm a.out
-t2:
-	g++ -std=c++17 -Wall main.cpp
-	a.out < in2
-	rm a.out
-t3:
-	g++ -std=c++17 -Wall main.cpp
-	a.out < in3
+
+t:
+	g++ -g -std=c++17 -Wall main.cpp
+	./a.out < in1
+	./a.out < in2
+	./a.out < in3
 	rm a.out
--- main.cpp
+++ main.cpp
@@ -3,8 +3,56 @@
 #define ll long long
 #define ld long double
 
-int main ()
+using namespace std;
+
+void do_task()
 {
+	string in, key; ll n;
+	cin >> in;
+	cin >> n;
+	cin >> key;
+	sort(key.begin(), key.end());
+	vector<pair<int, char>> vpair;
+	for (ll i = 0; i < (ll)in.length(); i++) {
+		if (in[i] == '?') {
+			vpair.push_back({ i, i + 1 < (ll)in.length() ? in[i + 1] : 'z' });
+		}
+	}
+	ll vpsz = vpair.size();
+	if (vpsz == n) {
+		for (auto i : vpair) {
+			in[i.first] = key[0];
+			key.erase(key.begin());
+		}
+		cout << in << "\n";
+	}
+	else {
+		ll extra = n - vpsz; 
+		for ( ll i=0; i<vpsz ; i++ ) {
+			in[vpair[i].first] = key[0];
+			key.erase(key.begin());
+			while ( extra ) {
+				if ( key[0] <= vpair[i].second ) {
+					in.insert ( vpair[i].first + 1, key.substr(0,1) );
+					key.erase ( key.begin() );
+				}
+			}
+		}
+		//if ( key.length() != 0 ) in += key;
+		cout << in << "\n";
+	}
+}
+
+int main()
+{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+
+	ll n = 1;
+	cin >> n;
+	while (n--) {
+		do_task();
+	}
 
 	return 0;
 }
