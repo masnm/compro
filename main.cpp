f9cb2bf4a001fe76399425ce0ff7d198782ea508
Ahmmed Sakib Noman <noman@localhost.localdomain>
2021-01-10 13:44:48 +0600

    Tryed coin change

--- main.cpp
+++ main.cpp
@@ -5,33 +5,59 @@ using namespace std;
 #define ll long long
 #define ld long double
 
+#define sl(a) scanf("%lld",&a)
+#define pl(a) printf("%lld ",a)
+#define nln printf("\n")
+
+const long long INF = 1e18;
+const long long mod = 1000000007;
+
+#warning need array limit
+ll A[long(1e7+5)];
+ll B[long(1e7+5)];
+
 void prepare_lookup_table ()
 {
 }
 
 void do_task ()
 {
-	ll n,x,sum = 0; cin >> n >> x;
-	vector<ll> a(n),b(n);
-	for ( ll i = 0 ; i < n ; i++ ) {
-		cin >> a[i];
-		b[i] = a[i];
-		sum += a[i];
+	ll n,m; sl(n); sl(m);
+	ll arr[n+1][m] = { };
+	vector<ll> c(m);
+	//for ( ll i = 0 ; i < m ; i++ ) arr[0][i] = 1
+	for ( ll i = 0 ; i < m ; i++ ) sl(c[i]);
+	for ( ll i = 0 ; i < 1 ; i++ ) {
+		for ( ll j = 0 ; j < m ; j++ ) {
+			for ( ll k = j ; k < m ; k++ ) {
+				if ( i + c[k] < n + 1 ) {
+					arr[i+c[k]][k]++;
+				}
+			}
+		}
 	}
-	ll index = 0;
-	while ( b[index] % x == 0 ) {
-		sum += a[index];
-		b[index] /= x;
-		index++;
-		if ( index == n ) index = 0;
+	for ( ll i = 1 ; i < n+1 ; i++ ) {
+		for ( ll j = 0 ; j < m ; j++ ) {
+			for ( ll k = j ; k < m ; k++ ) {
+				if ( i + c[k] < n + 1 && arr[i][k] != 0 ) {
+					arr[i+c[k]][k]++;
+				}
+			}
+		}
 	}
-	cout << sum << endl;
+	for ( ll i = 0 ; i < n+1 ; i++ ) {
+		for ( ll j = 0 ; j < m ; j++ ) {
+			pl(arr[i][j]);
+		}
+		nln;
+	}
+	nln;
 }
 
 int main ()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
 
 	prepare_lookup_table();
 
