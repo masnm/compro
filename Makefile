5c9390287f6c799ad2c4e8857a693c8c3353383e
masnm <nomanahmmed0@gmail.com>
2021-01-24 21:33:34 +0600

    last update

--- Makefile
+++ Makefile
@@ -1,33 +1,59 @@
 all:
-	cat Makefile
+	@cat Makefile
 reset:
-	rm in*
-	touch in1 in2 in3
-	echo "0 0 0" > in2
-	echo "0 0 0" > in3
-	cp template main.cpp
-	clear
+	@touch in9
+	@rm in*
+	@if [ -s a.out ]; then\
+		rm a.out;\
+	fi
+	@cp template main.cpp
+	@clear
 c:
-	g++ -std=c++17 -Wall main.cpp
-	rm a.out
+	@g++ -std=c++17 -Wall main.cpp
+	@rm a.out
 r:
-	g++ -std=c++17 -Wall main.cpp
-	./a.out
-	rm a.out
+	@g++ -std=c++17 -Wall main.cpp
+	@./a.out
+	@rm a.out
 d:
-	g++ -g -lm -std=c++17 -Wall -o a.out main.cpp
-	gdb ./a.out
-	rm a.out
-
+	@g++ -g -lm -std=c++17 -Wall -o a.out main.cpp
+	@gdb ./a.out
+	@rm a.out
 t:
-	g++ -Wall main.cpp -o a.out -std=c++17
-	time ./a.out < in1
-	time ./a.out < in2
-	time ./a.out < in3
-	rm a.out
+	@g++ -std=c++17 -Wshadow -Wall -o a.out main.cpp -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -g
+	@if [ -s in1 ]; then\
+		echo "Output one :";\
+		./a.out < in1;\
+	fi
+	@if [ -s in2 ]; then\
+		echo "Output two :";\
+		./a.out < in2;\
+	fi
+	@if [ -s in3 ]; then\
+		echo "Output three :";\
+		./a.out < in3;\
+	fi
+	@rm a.out
+tm:
+	@g++ -O2 -std=c++17 -Wno-unused-result -Wshadow -Wall -o a.out main.cpp
+	@if [ -s in1 ]; then\
+		echo "Output one :";\
+		time ./a.out < in1;\
+	fi
+	@if [ -s in2 ]; then\
+		echo "Output two :";\
+		time ./a.out < in2;\
+	fi
+	@if [ -s in3 ]; then\
+		echo "Output three :";\
+		time ./a.out < in3;\
+	fi
+	@rm a.out
 stc:
-	cat in*
+	@clear
+	@cat in*
 git:
-	git add .
-	git commit -S 
-	git push
+	@git add .
+	@git commit -S 
+	@git push
+	@clear
--- main.cpp
+++ main.cpp
@@ -2,151 +2,66 @@
 
 using namespace std;
 
-#define ll long long
-#define ld long double
-
-#define sl(a) scanf("%lld",&a)
-#define pl(a) printf("%lld ",a)
-#define nln printf("\n")
-
-const long long INF = 1e18;
-const long long mod = 1000000007;
-
-//#warning need array limit
-//ll A[long(1e7+5)];
-//ll B[long(1e7+5)];
-
 void prepare_lookup_table ()
 {
 }
 
-ld find_y ( ld x )
-{
-	ld expo = 2.718281828;
-	#warning Put your function on the next line
-	// Your equation goes here.. 
-	// the equation is used  "(X^3+X^2+X+7)"
-	ld ans = ( x * x * x ) - ( x * x ) - 1;
-	return ans;
-}
-
-ld absf ( ld a ) { return a < 0.0 ? -a : a; }
-
-bool need_to_iterate ( ld a, ld b, ld c )
-{
-	a = absf ( a );
-	b = absf ( b );
-	ld x = a > b ? a-b : b-a;
-	return c <= x;
-}
-
-ld calculate_mid ( ld x2, ld x1 )
-{
-	ld mid = ( ( x1 * find_y(x2) ) - ( x2 * find_y(x1) ) ) / ( find_y(x2) - find_y(x1) );
-	return mid;
-}
-
-bool found_values ( ld &m, ld &p, ld d, ld s )
-{
-	m = s;
-	p = m + d;
-	if ( find_y(m) < 0.0 && find_y(p) > 0.0 ) {
-		return true;
-	}
-	if ( find_y(m) > 0.0 && find_y(p) < 0.0 ) {
-		swap ( m, p );
-		return true;
-	}
-	return false;
-}
-
-void find_values_for_pm ( ld& plus, ld& minus )
-{
-	plus = 0.0; minus = 0.0;
-	ld des = 1.0, start = -100;
-	while ( plus == 0.0 && minus == 0.0 && des <= 10.0 ) {
-		start = -100;
-		while ( start < 101 ) {
-			start += des;
-			if ( found_values ( minus, plus, des, start ) ) {
-				cout << "Negative : " << minus << " " << find_y(minus) << "\n";
-				cout << "Positive : " << plus << " " << find_y(plus) << "\n";
-				break;
-			}
-			minus = plus = 0.0;
-		}
-		des += 0.1;
-	}
-}
+struct st {
+	int sum = 0;
+	int mn = INT_MAX;
+	int mx = INT_MIN;
+};
 
 void do_task ()
 {
-	ld value_for_plus, value_for_minus;
-
-	#warning Initialize the next two variables
-	// initialuze the next two variables with the their name suggested.
-//	value_for_plus = 1.0;
-//	value_for_minus = 0.0;
-	find_values_for_pm ( value_for_plus, value_for_minus );
-	if ( value_for_plus == 0.0 && value_for_minus == 0.0 ) {	
-		cout << "Failed to find the values\n"
-			"please enter the values you would like to use\n";
-		cout << "Minus : "; cin >> value_for_minus;
-		cout << "Plus : "; cin >> value_for_plus;
+	int n,m; cin >> n >> m;
+	vector<vector<pair<int,int>>> gp(n+1);
+	int s,e,w;
+	while ( m-- ) {
+		cin >> s >> e >> w;
+		gp[s].emplace_back(e,w);
+		gp[e].emplace_back(s,w);
 	}
-//	value_for_plus = 1.0;
-//	value_for_minus = 0.5;
-	// replace the 'X' from the next line with the limit of accuracy
-
-	#warning Put the precission limit
-	// don't delete the char 'e'. 'e' is nacessary
-	ld precission = (1.0/2.0) * (1.0/(1e4));
-
-	cout << "Itera." << "\t" <<
-		"Minus value." << "\t" <<
-		"Plus value." << "\t" <<
-		"Mid" << "\t" <<
-		"f(x)" << endl;
-
-	ll iteration = 1;
-	while ( true ) {
-		if ( !need_to_iterate ( value_for_plus, value_for_minus, precission ) ) {
-			break;
+	bool visited[n+1] = { };
+	queue<int> q; q.push(1); visited[1] = true;
+	st out[n+1];
+	out[1].sum = 0;
+	while ( !q.empty() ) {
+		int top = q.front(); q.pop();
+		for ( auto i : gp[top] ) {
+			if ( i.first != 1 && !visited[i.first] ) {
+				out[i.first].sum = out[top].sum + i.second;
+				out[i.first].mn = min ( out[top].mn, i.second );
+				out[i.first].mx = max ( out[top].mx, i.second );
+				q.push(i.first);
+				visited[i.first] = true;
+			} else if ( i.first != 1 ) {
+				int tsm = out[top].sum + i.second,
+				    tmx = max ( out[top].mx, i.second ),
+				    tmn = min ( out[top].mn, i.second );
+				if ( (out[i.first].sum-out[i.first].mx+out[i.first].mn) > (tsm-tmx+tmn) ) {
+					out[i.first].sum = tsm;
+					out[i.first].mn = tmn;
+					out[i.first].mx = tmx;
+					q.push(i.first);
+				}
+			}
 		}
-
-		ld mid = calculate_mid ( value_for_plus, value_for_minus );
-
-		ld y = find_y ( mid );
-
-		cout << iteration++ << "\t" <<
-			value_for_minus << "\t" <<
-			value_for_plus << "\t" <<
-			mid << "\t" <<
-			y << endl;
-
-		if ( y < 0.0 ) {
-			value_for_minus = mid;
-		} else if ( y > 0.0 ) {
-			value_for_plus = mid;
-		} else {
-			value_for_plus = value_for_minus = mid;
-		}
-
-		// if you don't want to press enter for every iteration
-		// just delete the next line 
-		cin.get();
+	}
+	for ( int i = 2 ; i <= n ; i++ ) {
+		cout << out[i].sum - out[i].mx + out[i].mn << " ";
 	}
 }
 
 int main ()
 {
-//	ios::sync_with_stdio(false);
-//	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 
 	prepare_lookup_table();
 
-	ll t = 1;
-//	sl(t);
+	int t = 1;
+	//cin >> t;
 	while ( t-- ) {
 		do_task();
 	}
