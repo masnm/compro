06a498ee7567b738c9236582374ff3483db7c036
masnm <nomanahmmed0@gmail.com>
2021-04-11 16:37:02 +0600

    Added working MCM

--- main.cpp
+++ main.cpp
@@ -1,96 +1,78 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 #define endl '\n'
 typedef long double ld;
 typedef long long int ll;
 typedef unsigned long long ull;
- 
+
 template<typename T> void chmax ( T& a, T b ) { if ( a < b ) a = b; }
 template<typename T> void chmin ( T& a, T b ) { if ( a > b ) a = b; }
- 
+
 void prepare_lookup_table ()
 {
 }
- 
-struct st {
-	int d;
-	int v;
-};
- 
+
 void do_task ()
 {
-	string s, t; cin >> s >> t;
-	int sl = s.length(), tl = t.length();
-	vector<vector<st>> dp(sl+1, vector<st>(tl+1));
-	for ( int i = 0 ; i < 1 ; ++i ) {
-		for ( int j = 0 ; j < sl+1 ; ++j ) {
-			dp[j][i] = { -1, 0 };
-		}
-		for ( int j = 0 ; j < tl+1 ; ++j ) {
-			dp[i][j] = { -1, 0 };
-		}
-	}
-	int left = 0, up = 1, cor = 2;
-	for ( int i = 1 ; i < sl+1 ; ++i ) {
-		for ( int j = 1 ; j < tl+1 ; ++j ) {
-			if ( s[i-1] == t[j-1] ) {
-				dp[i][j] = { cor, dp[i-1][j-1].v+1 };
-			} else if ( dp[i][j-1].v > dp[i-1][j].v ) {
-				dp[i][j] = { left, dp[i][j-1].v };
-			} else {
-				dp[i][j] = { up, dp[i-1][j].v };
+	ll n; cin >> n;
+	vector<ll> v(n);
+	for ( auto& i : v ) cin >> i;
+	--n;
+	vector<vector<ll>> cost(n+1,vector<ll>(n+1,0)), mink(n+1,vector<ll>(n+1,0));
+	for ( ll i = 2 ; i <= n ; ++i ) {
+		for ( ll j = 1 ; j <= n-i+1 ; ++j ) {
+			ll ind = j+i-1;
+			cost[j][ind] = LLONG_MAX;
+			for ( ll k = j ; k < ind ; ++k ) {
+				ll t = cost[j][k] + cost[k+1][ind] + v[j-1]*v[k]*v[ind];
+				if ( t < cost[j][ind] ) {
+					cost[j][ind] = t;
+					mink[j][ind] = k;
+				}
 			}
 		}
 	}
-	// starting printing table
-	cout << endl << "Printing Table" << endl;
-	for ( int i = 0 ; i < sl+1 ; ++i ) {
-		for ( int j = 0 ; j < tl+1 ; ++j ) {
-			int dir = dp[i][j].d;
-			int value = dp[i][j].v;
-			if ( dir == left ) {
-				cout << "←";
-			} else if ( dir == up ) {
-				cout << "↑";
-			} else if ( dir == cor ) {
-				cout << "↖";
-			} else {
-				cout << "⥀";
-			}
-			cout << value << " ";
+	string s = "";
+	for ( auto& i : cost ) {
+		for ( auto& j : i ) {
+			s = to_string(j); reverse(s.begin(),s.end());
+			ll llen = 6-s.length();
+			while ( llen-- ) { s += ' '; }
+			reverse(s.begin(),s.end());
+			cout << s << " ";
 		}
 		cout << endl;
 	}
 	cout << endl;
-	// end printing table
-	string ans = "";
-	int i = sl, j = tl, direction = dp[sl][tl].d;
-	while ( direction != -1 ) {
-		if ( direction == cor ) {
-			ans += s[i-1];
-			--i; --j;
-		} else if ( direction == up ) {
-			--i;
-		} else {
-			--j;
+	for ( auto& i : mink ) {
+		for ( auto& j : i ) {
+			s = to_string(j); reverse(s.begin(),s.end());
+			ll llen = 3-s.length();
+			while ( llen-- ) { s += ' '; }
+			reverse(s.begin(),s.end());
+			cout << s << " ";
 		}
-		direction = dp[i][j].d;
+		cout << endl;
 	}
-	reverse(ans.begin(), ans.end());
-	cout << "The LCS is : " << ans << endl;
+	cout << endl;
 }
- 
+
 int main ()
 {
-//	ios_base::sync_with_stdio(false);
-//	cin.tie(0);
- 
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
 	prepare_lookup_table();
- 
-	do_task();
- 
- 
+
+	int t = 1;
+	for ( int i = 1 ; i <= t ; ++i ) {
+		//cout << "Case " << i << ": " ;
+		do_task();
+	}
+
+
 	return 0;
 }
+// resize-pane -L 30 
