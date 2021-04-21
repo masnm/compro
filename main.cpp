e9b6f347fd6ddbd9a19aa2d794ade7fde5228605
noman <nomanahmmed0@gmail.com>
2021-04-21 20:56:16 +0600

    717 a

--- main.cpp
+++ main.cpp
@@ -14,129 +14,28 @@ void prepare_lookup_table ()
 {
 }
 
-const ll queen = INT_MAX;
-
-void print ( vector<vector<int>>& v )
-{
-	for ( auto& i : v ) {
-		for ( auto& j : i ) {
-			cout << (j==queen?'Q':'.') << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
-
-void downgrade ( vector<vector<int>>& v, int x, int y, int n )
-{
-	for ( ll i = 0 ; i < n ; ++i ) {
-		if ( v[x][i] != queen ) --v[x][i];
-		if ( v[i][y] != queen ) --v[i][y];
-	}
-	for ( ll i = 1 ; i <= n ; ++i ) {
-		if ( x+i < n && y+i < n ) {
-			--v[x+i][y+i];
-		} else {
-			break;
-		}
-	}
-	for ( ll i = 1 ; i <= n ; ++i ) {
-		if ( x-i > -1 && y-i > -1 ) {
-			--v[x-i][y-i];
-		} else {
-			break;
-		}
-	}
-	for ( ll i = 1 ; i <= n ; ++i ) {
-		if ( x-i > -1 && y+i < n ) {
-			--v[x-i][y+i];
-		} else {
-			break;
-		}
-	}
-	for ( ll i = 1 ; i <= n ; ++i ) {
-		if ( x+i < n && y-i > -1 ) {
-			--v[x+i][y-i];
-		} else {
-			break;
-		}
-	}
-}
-
-void upgrade ( vector<vector<int>>& v, int x, int y, int n )
-{
-	for ( ll i = 0 ; i < n ; ++i ) {
-		if ( v[x][i] != queen ) ++v[x][i];
-		if ( v[i][y] != queen ) ++v[i][y];
-	}
-	for ( ll i = 1 ; i <= n ; ++i ) {
-		if ( x+i < n && y+i < n ) {
-			++v[x+i][y+i];
-		} else {
-			break;
-		}
-	}
-	for ( ll i = 1 ; i <= n ; ++i ) {
-		if ( x-i > -1 && y-i > -1 ) {
-			++v[x-i][y-i];
-		} else {
-			break;
-		}
-	}
-	for ( ll i = 1 ; i <= n ; ++i ) {
-		if ( x-i > -1 && y+i < n ) {
-			++v[x-i][y+i];
-		} else {
-			break;
-		}
-	}
-	for ( ll i = 1 ; i <= n ; ++i ) {
-		if ( x+i < n && y-i > -1 ) {
-			++v[x+i][y-i];
-		} else {
-			break;
-		}
-	}
-}
-
-bool solve ( vector<vector<int>>& v, int ind, int& n )
-{
-	if ( ind == n ) {
-		cout << "Found solution :) " << endl;
-		return true;
-	}
-	for ( ll i = 0 ; i < n ; ++i ) {
-		if ( !v[ind][i] ) {
-			v[ind][i] = queen;
-			upgrade ( v, ind, i, n );
-
-			cout << "Going to row " << ind+1 << endl;
-			print ( v );
-
-			bool ans = solve ( v, ind+1, n );
-			if ( ans ) {
-				return true;
-			}
-
-			downgrade ( v, ind, i, n );
-			v[ind][i] = 0;
-		}
-	}
-	print ( v );
-	cout << "Failed in row " << ind+1 << " . So, " << endl;
-	cout << "Back tracking." << endl << endl;
-	return false;
-}
-
 void do_task ()
 {
-	int n; cin >> n;
-	vector<vector<int>> v(n, vector<int>(n,0));
-	if ( solve ( v, 0, n ) ) {
-		print ( v );
-	} else {
-		cout << "No solution." << endl;
+	ll n, k; cin >> n >> k;
+	vector<ll> v(n);
+	ll sm = 0;
+	for ( auto& i : v ) {
+		cin >> i;
+		if ( k > 0 ) {
+			if ( i > k ) {
+				sm += k;
+				i -= k;
+				k = 0;
+			} else {
+				sm += i;
+				k -= i;
+				i = 0;
+			}
+		}
 	}
+	v[n-1] += sm;
+	for ( auto& i : v ) cout << i << " ";
+	cout << endl;
 }
 
 int main ()
@@ -147,6 +46,7 @@ int main ()
 	prepare_lookup_table();
 
 	int t = 1;
+	cin >> t;
 	for ( int i = 1 ; i <= t ; ++i ) {
 		//cout << "Case " << i << ": " ;
 		do_task();
