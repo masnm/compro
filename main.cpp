8b404c3b762e8c5a808ad60a67550c94e13662bb
masnm <nomanahmmed0@gmail.com>
2021-04-29 15:54:55 +0600

    POI problem solution

--- main.cpp
+++ main.cpp
@@ -16,6 +16,34 @@ void prepare_lookup_table ()
 
 void do_task ()
 {
+	ll n, t, p; cin >> n >> t >> p; --p;
+	vector<vector<ll>> v(n,vector<ll>(t));
+	vector<pair<pair<ll,ll>,ll>> vpp;
+	ll sc[t] = { };
+	for ( ll i = 0 ; i < n ; ++i ) {
+		ll cnt = 0;
+		for ( ll j = 0 ; j < t ; ++j ) {
+			cin >> v[i][j];
+			if ( v[i][j] ) ++sc[j], ++cnt;
+		}
+		vpp.emplace_back ( make_pair( make_pair( -1, cnt ), n-i ) );
+	}
+	for ( ll i = 0 ; i < t ; ++i ) sc[i] = n - sc[i];
+	for ( ll i = 0 ; i < n ; ++i ) {
+		ll scr = 0;
+		for ( ll j = 0 ; j < t ; ++j ) {
+			scr += v[i][j] ? sc[j] : 0;
+		}
+		vpp[i].first.first = scr;
+	}
+	sort ( vpp.begin(), vpp.end() );
+	for ( ll i = n - 1 ; i > -1 ; --i ) {
+		if ( n - vpp[i].second == p ) {
+			cout << vpp[i].first.first << " " << n - vpp[i].second + 1 << endl;
+			return;
+		}
+	}
+	assert(false);
 }
 
 int main ()
@@ -26,7 +54,6 @@ int main ()
 	prepare_lookup_table();
 
 	int t = 1;
-	cin >> t;
 	for ( int i = 1 ; i <= t ; ++i ) {
 		//cout << "Case " << i << ": " ;
 		do_task();
