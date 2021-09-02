d8f572cfe7bbd9eee24e8081bf5b8180f418f28a
Ahmmed Sakib Noman <nomanahmmed0@gmail.com>
2021-09-02 21:11:57 +0600

    command script added

--- Scripts/m_bash_sync.sh
+++ Scripts/m_bash_sync.sh
@@ -5,10 +5,12 @@ sudo cp my_gdb_debug_cmnd.sh /bin/my_gdb_debug_cmnd.sh
 sudo cp my_verdict_cmnd.sh /bin/my_verdict_cmnd.sh
 sudo cp my_input_submit_cmnd.sh /bin/my_input_submit_cmnd.sh
 sudo cp my_accepted_wrong_ans_cmnd.sh /bin/my_accepted_wrong_ans_cmnd.sh
+sudo cp my_code_compile_cmnd.sh /bin/my_code_compile_cmnd.sh
 
 echo -e '\e[0;33m#___ Dont forgate to make aliases___#\e[m'
 echo 'alias dn="my_done_cmnd.sh"'
 echo 'alias d="my_gdb_debug_cmnd.sh"'
 echo 'alias t="my_verdict_cmnd.sh"'
-echo 'alias v="my_accepted_wrong_ans_cmnd.sh"'
 echo 'alias is="my_input_submit_cmnd.sh"'
+echo 'alias v="my_accepted_wrong_ans_cmnd.sh"'
+echo 'alias c="my_code_compile_cmnd.sh"'
